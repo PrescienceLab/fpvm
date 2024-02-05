945a1d12d20785accb1a223ab0e34ea1503c67cd
Peter Dinda <pdinda@northwestern.edu>
2024-02-04 19:19:40 -0600

    Add various scripts for gdb-based debugging, etc

--- scripts/fpvm_run.sh
+++ scripts/fpvm_run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z "${FPVM_HOME}" ]; then
+	echo "Set FPVM_HOME";
+	exit -1
+fi	
 
-LD_PRELOAD=build/fpvm.so FPVM_DISABLE_PTHREADS=y FPVM_AGGRESSIVE=y $@
+if [ ! -e $FPVM_HOME/build/fpvm.so  ] ; then
+	echo "$FPVM_HOME/build/fpvm.so does not exist - build first!"
+	exit -1
+fi
+
+LD_PRELOAD=$FPVM_HOME/build/fpvm.so FPVM_AGGRESSIVE=y $@
 
---
+++ scripts/gdb_it.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gdb -ix scripts/gdb-init $@
+
+
