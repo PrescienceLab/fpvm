7fb88aeb269fe2914a964659b156c6e4426e2112
Nick Wanninger <nickwanninger@gmail.com>
2024-03-29 10:55:11 -0500

    test/run_all.sh: save results to a date-indexed location

--- test/run_all.sh
+++ test/run_all.sh
@@ -4,7 +4,15 @@ export FPVM_HOME=$(realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null
 source ${FPVM_HOME}/ENV
 
 
-mkdir -p ${FPVM_HOME}/results
+
+RESULTS=${FPVM_HOME}/results/`date +"%d-%m-%Y-%H%M%S"`
+
+
+rm -f ${FPVM_HOME}/results/latest
+ln -s ${RESULTS} ${FPVM_HOME}/results/latest
+
+
+mkdir -p ${RESULTS}
 NAS_CLASS=S
 
 buildstep() {
@@ -18,31 +26,47 @@ function run_test {
   binary=$2
 
   echo $name $binary
-  buildstep ${name} fpvm benchmark -c 1 --output=${FPVM_HOME}/results/${name} ${binary}
+  buildstep ${name} fpvm benchmark -c 1 --output=${RESULTS}/${name} ${binary}
+}
+
+function maybe_rebuild {
+  BINARY=$1
+  shift
+
+  # Don't recompile if you don't need to
+  if [ ! -e ${BINARY} ]; then
+    "$@"
+  else
+    echo "Not recompiling ${BINARY}. It already exists"
+  fi
 }
 
 
-pushd ${FPVM_HOME}/test/nas
-  make clean
-  cp config/make-gcc.def config/make.def
 
-  for b in mg bt sp lu ft is cg ep; do
-    make ${b^^} CLASS=$NAS_CLASS
-    mkdir -p bin/${b}
-    mv bin/${b}.${NAS_CLASS} bin/${b}/${b}
-  done
+# pushd ${FPVM_HOME}/test/nas
+#   make clean
+#   cp config/make-gcc.def config/make.def
+#
+#   for b in mg bt sp lu ft is cg ep; do
+#     # Don't recompile if you don't need to
+#     if [ ! -e bin/${b}/${b} ]; then
+#       make ${b^^} CLASS=$NAS_CLASS
+#       mkdir -p bin/${b}
+#       mv bin/${b}.${NAS_CLASS} bin/${b}/${b}
+#     fi
+#   done
+#
+#
+#   for b in mg bt sp lu ft is cg ep; do
+#     run_test "nas_${b}" bin/${b}/${b}
+#   done
+# popd
 
 
-  for b in mg bt sp lu ft is cg ep; do
-    run_test "nas_${b}" bin/${b}/${b}
-  done
-popd
-
-
-
-make -C ${FPVM_HOME}/test/lorenz lorenz_attractor
+maybe_rebuild test/lorenz/lorenz_attractor make -C ${FPVM_HOME}/test/lorenz lorenz_attractor
 run_test lorenz ${FPVM_HOME}/test/lorenz/lorenz_attractor
 
+exit
 
 make -C ${FPVM_HOME}/test/double_pendulum double_pendulum
 run_test double_pendulum ${FPVM_HOME}/test/double_pendulum/double_pendulum
