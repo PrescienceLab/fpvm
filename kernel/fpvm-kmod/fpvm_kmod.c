7e66f198b6d9e43bb1765e1877c0f2fa98d827a2
Nadharm <nadharmdhiantravan@gmail.com>
2024-03-30 21:12:34 +0000

    kmod: default path behavior (todo: non-breaking slowdown bug)

--- kernel/fpvm-kmod/fpvm_kmod.c
+++ kernel/fpvm-kmod/fpvm_kmod.c
@@ -11,6 +11,7 @@
 #include <linux/smp.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/entry-common.h>
 
 #include <asm/io.h>
 #include <asm/current.h>
@@ -18,6 +19,7 @@
 #include <asm/desc_defs.h>
 #include <asm/cpu_entry_area.h>
 #include <asm/page.h>
+#include <asm/apic.h>
 
 #include <uapi/asm-generic/errno-base.h>
 
@@ -32,6 +34,19 @@ MODULE_PARM_DESC(fpvm_error_entry, "Address of the `fpvm_error_entry` symbol");
 unsigned long fpvm_error_return;
 module_param_named(fpvm_error_return, fpvm_error_return, ulong, 0);
 MODULE_PARM_DESC(fpvm_error_return, "Address of the `fpvm_error_return` symbol");
+
+unsigned long fpvm_math_error;
+module_param_named(fpvm_math_error, fpvm_math_error, ulong, 0);
+MODULE_PARM_DESC(fpvm_math_error, "Address of the `fpvm_math_error` symbol");
+
+unsigned long fpvm_irqentry_enter;
+module_param_named(fpvm_irqentry_enter, fpvm_irqentry_enter, ulong, 0);
+MODULE_PARM_DESC(fpvm_irqentry_enter, "Address of the `fpvm_irqentry_enter` symbol");
+
+unsigned long fpvm_irqentry_exit;
+module_param_named(fpvm_irqentry_exit, fpvm_irqentry_exit, ulong, 0);
+MODULE_PARM_DESC(fpvm_irqentry_exit, "Address of the `fpvm_irqentry_exit` symbol");
+
 
 unsigned long error_entry;
 unsigned long error_return;
@@ -63,55 +78,58 @@ static struct list_head upi_list = LIST_HEAD_INIT(upi_list);
 static struct user_proc_info * upi_find(pid_t target_pid) {
     struct user_proc_info * cur = NULL;
     struct user_proc_info * n = NULL;
-	int found = 0;
-    
+    int found = 0;
+
     // BUG?: deletion from list by another thread?
     // TODO: add read-write lock
     list_for_each_entry_safe(cur, n, &upi_list, node) {
-		if (cur->pid == target_pid) {
-			found = 1;
-			break; 
-		}
-    }
-	if (found) {
-		return cur;
+	if (cur->pid == target_pid) {
+	    found = 1;
+	    break; 
 	}
-	return NULL;
+    }
+    if (found) {
+	return cur;
+    }
+    return NULL;
 }
 
 static void upi_add(struct user_proc_info * upi) {
-	list_add(&upi->node, &upi_list);
+    list_add(&upi->node, &upi_list);
 }
 
 static void upi_del(struct user_proc_info * upi) {
-	list_del(&upi->node);
+    list_del(&upi->node);
 }
 
 void the_fpvm_hook(struct pt_regs* regs)
 {    
     struct user_proc_info * upi = upi_find(current->pid);
-
     if (upi) {
-        /* saving RIP to stack */
-        uint64_t old_sp = regs->sp;
-        uint64_t state[2] = {old_sp, regs->ip};
-        regs->sp -= 152;
-        // Because we need to do alignment, I need to give myself a little buffer
-        // for copying the saved RIP
-        
-        regs->sp -= 0x8;
-        regs->sp &= 0xFFFFFFFFFFFFFFF0; // This alignment is why I need to copy the RIP 2x
-        
-        // Copy %rip here for our state save area
-        regs->sp -= 0x10;
-        if (copy_to_user((void *)regs->sp, &state, sizeof(state))){
-            printk("copy to user..\n");
-        }
+	/* saving RIP to stack */
+	uint64_t old_sp = regs->sp;
+	uint64_t state[2] = {old_sp, regs->ip};
+	regs->sp -= 152;
+	// Because we need to do alignment, I need to give myself a little buffer
+	// for copying the saved RIP
 
-        /* setting RIP to user proc's handler */
-        regs->ip = (long unsigned int) upi->user_handler;
-        return;
+	regs->sp -= 0x8;
+	regs->sp &= 0xFFFFFFFFFFFFFFF0; // This alignment is why I need to copy the RIP 2x
+
+	// Copy %rip here for our state save area
+	regs->sp -= 0x10;
+	if (copy_to_user((void *)regs->sp, &state, sizeof(state))){
+	    printk("copy to user..\n");
+	}
+
+	/* setting RIP to user proc's handler */
+	regs->ip = (long unsigned int) upi->user_handler;
+	return;
     }
+
+    irqentry_state_t state = ((irqentry_state_t (*)(struct pt_regs*))fpvm_irqentry_enter)(regs);
+    ((void (*)(struct pt_regs*, int))fpvm_math_error)(regs, X86_TRAP_XF);
+    ((void (*)(struct pt_regs*, irqentry_state_t))fpvm_irqentry_exit)(regs, state);
     return;
 }
 
@@ -119,29 +137,29 @@ EXPORT_SYMBOL(the_fpvm_hook);
 
 static void fpvm_remove_handlers(pid_t target) {
     struct user_proc_info * upi = upi_find(target);
-	mutex_lock_interruptible(&fpvm_dev_lock);
-	while (upi) {
-		upi_del(upi);   
-		kfree(upi);
-		upi = upi_find(target);
-	}
-	mutex_unlock(&fpvm_dev_lock);
-	printk("Removed all handlers for %ld\n", (long int)target);
-	return;
+    mutex_lock_interruptible(&fpvm_dev_lock);
+    while (upi) {
+	upi_del(upi);   
+	kfree(upi);
+	upi = upi_find(target);
+    }
+    mutex_unlock(&fpvm_dev_lock);
+    printk("Removed all handlers for %ld\n", (long int)target);
+    return;
 }
 
 static void fpvm_add_handler(void * handle_func) {
-	struct user_proc_info * new_upi;
-	mutex_lock_interruptible(&fpvm_dev_lock);
+    struct user_proc_info * new_upi;
+    mutex_lock_interruptible(&fpvm_dev_lock);
 
-	new_upi = kmalloc(sizeof(struct user_proc_info), GFP_KERNEL);
-	new_upi->user_handler = handle_func;
-	new_upi->pid = current->pid;
-	upi_add(new_upi);
+    new_upi = kmalloc(sizeof(struct user_proc_info), GFP_KERNEL);
+    new_upi->user_handler = handle_func;
+    new_upi->pid = current->pid;
+    upi_add(new_upi);
 
-	mutex_unlock(&fpvm_dev_lock);
-	printk("PID %ld registered handler at %px\n", (long int)new_upi->pid, new_upi->user_handler);
-	return;
+    mutex_unlock(&fpvm_dev_lock);
+    printk("PID %ld registered handler at %px\n", (long int)new_upi->pid, new_upi->user_handler);
+    return;
 }
 
 /* 
@@ -168,49 +186,49 @@ static int fpvm_release(struct inode *inode, struct file *file)
 static long fpvm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     switch(cmd) {
-        case FPVM_IOCTL_REG:
-			fpvm_add_handler((void *)arg);
-            break;
-        case FPVM_IOCTL_UNREG:
-            fpvm_remove_handlers(current->pid);
-            break;
-        default:
-            printk("Invalid FPVM IOCTL\n");
-            return -EINVAL;
-        }
+	case FPVM_IOCTL_REG:
+	    fpvm_add_handler((void *)arg);
+	    break;
+	case FPVM_IOCTL_UNREG:
+	    fpvm_remove_handlers(current->pid);
+	    break;
+	default:
+	    printk("Invalid FPVM IOCTL\n");
+	    return -EINVAL;
+    }
     return 0;
 }
 
 static struct file_operations fops = 
-{
-    .owner          = THIS_MODULE,
-    .open           = fpvm_open,
-    .unlocked_ioctl = fpvm_ioctl,
-    .release        = fpvm_release
-};
+    {
+	.owner          = THIS_MODULE,
+	.open           = fpvm_open,
+	.unlocked_ioctl = fpvm_ioctl,
+	.release        = fpvm_release
+    };
 
 static int create_fpvm_dev(void) {
-	int err = register_chrdev_region(MKDEV(MAJOR_NUM, 0), MAX_MINORS, "fpvm_device_driver");
+    int err = register_chrdev_region(MKDEV(MAJOR_NUM, 0), MAX_MINORS, "fpvm_device_driver");
 
     if (err != 0){
-        printk("[!] Unable to register FPVM Device Driver\n");
-        return err;
+	printk("[!] Unable to register FPVM Device Driver\n");
+	return err;
     }
 
     cdev_init(&fpvm_cdev, &fops);
     fpvm_class = class_create(THIS_MODULE, "fpvm_class");
     device_create(fpvm_class, NULL, MKDEV(MAJOR_NUM, 0), NULL, "fpvm_dev");
     cdev_add(&fpvm_cdev, MKDEV(MAJOR_NUM, 0), 1);
-	return 0;
+    return 0;
 }
 
 static int destroy_fpvm_dev(void) {
-	device_destroy(fpvm_class, MKDEV(MAJOR_NUM, 0));
+    device_destroy(fpvm_class, MKDEV(MAJOR_NUM, 0));
     class_destroy(fpvm_class);
     cdev_del(&fpvm_cdev);
     unregister_chrdev_region(MKDEV(MAJOR_NUM, 0), MAX_MINORS);
     printk("[*] FPVM Device Driver unregistered\n");
-	return 0;
+    return 0;
 }
 
 /* 
@@ -223,19 +241,19 @@ Helpers for IDT Manipulation
 
 static void force_write_cr0(unsigned long new_val) {
     asm __volatile__ (
-        "mov %0, %%rdi;"
-        "mov %%rdi, %%cr0;"
-        ::"m"(new_val)
-    );
+	    "mov %0, %%rdi;"
+	    "mov %%rdi, %%cr0;"
+	    ::"m"(new_val)
+	);
 }
 
 static unsigned long force_read_cr0(void) { 
     unsigned long cr0_val;
     asm __volatile__ (
-        "mov %%cr0, %%rdi;"
-        "mov %%rdi, %0;"
-        :"=m"(cr0_val)
-    );
+	    "mov %%cr0, %%rdi;"
+	    "mov %%rdi, %0;"
+	    :"=m"(cr0_val)
+	);
     return cr0_val;
 }
 
@@ -247,9 +265,9 @@ static struct desc_ptr get_idtr(void) {
 
 static uint64_t extract_handler_address(gate_desc * gd) {
     uint64_t handler;
-	handler = (uint64_t) gd->offset_low;
+    handler = (uint64_t) gd->offset_low;
     handler += (uint64_t) gd->offset_middle << 16;
-	handler += (uint64_t) gd->offset_high << 32;
+    handler += (uint64_t) gd->offset_high << 32;
     return handler;
 }
 
@@ -302,7 +320,7 @@ static void modify_idt(void) {
 
     // Profit
     printk("[!] IDT Modification done!\n");
-	return;
+    return;
 }
 
 // Called during our cleanup. Restore the original xf handler.
@@ -312,7 +330,7 @@ static void restore_idt(void) {
     gate_desc * XF_gate_desc;
 
     if (original_xf_handler == 0) {
-      return;
+	return;
     }
     IDTR = get_idtr();
     idt = (gate_desc *) IDTR.address;
@@ -335,34 +353,40 @@ Kernel Mod init + exit
 static int __init test_init(void)
 {   
     int err;
-	unsigned long flags;
+    unsigned long flags;
 
-	local_irq_save(flags);
+    local_irq_save(flags);
 
+    if (!fpvm_error_entry || !fpvm_error_return || !fpvm_math_error ||
+	!fpvm_irqentry_enter || !fpvm_irqentry_exit
+    ) {
+	printk("[X] fpvm_dev: invalid params\n");
+	return -1;
+    }
     /* replace default xf handler with _fpvm_idt_entry */
     modify_idt();
 
     /* register the device and stuff */
-  	err = create_fpvm_dev();
-	if (err) { return err; }
+    err = create_fpvm_dev();
+    if (err) { return err; }
 
     local_irq_restore(flags);
-	return 0;
+    return 0;
 }
 
 
 static void __exit test_exit(void)
 {
-	unsigned long flags;
-	local_irq_save(flags);
+    unsigned long flags;
+    local_irq_save(flags);
 
     /* restore original xf handler into idt */
     restore_idt();
 
     /* unregister the device */
-	destroy_fpvm_dev();
+    destroy_fpvm_dev();
 
-	local_irq_restore(flags);
+    local_irq_restore(flags);
 }
 
 module_init(test_init);
--- kernel/kmod_setup.sh
+++ kernel/kmod_setup.sh
@@ -3,7 +3,10 @@
 pushd fpvm-kmod
 sudo insmod fpvm_dev.ko \
     fpvm_error_entry=0x$(sudo grep ' error_entry' /proc/kallsyms | cut -d' ' -f1) \
-    fpvm_error_return=0x$(sudo grep ' error_return' /proc/kallsyms | cut -d' ' -f1) 
+    fpvm_error_return=0x$(sudo grep ' error_return' /proc/kallsyms | cut -d' ' -f1) \
+    fpvm_math_error=0x$(sudo grep ' math_error' /proc/kallsyms | cut -d' ' -f1) \
+    fpvm_irqentry_enter=0x$(sudo grep ' irqentry_enter' /proc/kallsyms | cut -d' ' -f1) \
+    fpvm_irqentry_exit=0x$(sudo grep ' irqentry_exit' /proc/kallsyms | cut -d' ' -f1) 
 
 pushd /dev
 sudo mknod fpvm_dev c 17 0
