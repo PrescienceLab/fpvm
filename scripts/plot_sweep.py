f90267bcac5aa823ce0e03ee4ed7fb38012909dc
Nick Wanninger <nickwanninger@gmail.com>
2024-12-20 11:38:12 -0600

    add plotting script and update the fpvm sweep script

--- scripts/plot_sweep.py
+++ scripts/plot_sweep.py
@@ -3,7 +3,7 @@ import os
 import pathlib
 from pathlib import Path
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 def add_column(df, name, value):
     df[name] = value
@@ -20,7 +20,7 @@ class BenchmarkResult:
 
     def imbue_df(self, df):
         df['benchmark'] = self.benchmark
-        df['alt'] = self.telem
+        df['alt'] = self.alt
         df['telem'] = self.telem
         df['config'] = self.config
 
@@ -31,6 +31,15 @@ class BenchmarkResult:
         ])
         self.imbue_df(df)
         return df
+
+    def read_telemetry(self):
+
+        try:
+            df = pd.read_csv(self.path/'fpvm_magic_amortized.csv')
+            self.imbue_df(df)
+            return df
+        except:
+            return None
 
 
 results = []
@@ -49,9 +58,57 @@ for benchmark_entry in Path('sweep-results').iterdir():
 
 
 
-
 rusages = pd.concat([result.read_rusages() for result in results])
+
 rusages.to_csv('sweep-results/rusage.csv', index=False)
+
+
+
 print(rusages)
 
-# First, gather all the rusage CSVs into sweep-results/rusage.csv
+def plot_overhead(ru_name):
+    ru = rusages[rusages['alt'] == 'boxed']
+    ru = ru[ru['telem'] == 'basic_timing']
+    r = ru.groupby(by=['config', 'telem', 'alt', 'benchmark', 'type']).mean().reset_index()
+    print(r)
+    oh = r.pivot(index=['benchmark', 'config'], columns='type', values=ru_name)
+    print(oh)
+    oh['overhead'] = (oh['fpvm'] - oh['baseline']) / oh['baseline']
+    oh.reset_index(inplace=True)
+    oh.to_csv(f'sweep-results/{ru_name}_overhead.csv', index=False)
+    plt.figure(figsize=(12, 8))  # Width of 12 and height of 8
+    g = sns.barplot(data=oh, x='benchmark', hue='config', y='overhead')
+    plt.legend(loc='lower left')
+
+    g.set(title=f'{ru_name} overhead')
+    plt.savefig(f'sweep-results/{ru_name}_overhead.pdf', format='pdf')
+
+
+plot_overhead('time')
+plot_overhead('stime')
+plot_overhead('utime')
+# plot_overhead('maxrss')
+# plot_overhead('minor')
+
+
+telemetry = []
+
+for result in results:
+    t = result.read_telemetry()
+    if t is None:
+        continue
+    telemetry.append(t)
+
+print(telemetry)
+tel = pd.concat(telemetry)
+tel.to_csv('sweep-results/telemetry.csv', index=False)
+
+
+
+
+t = pd.melt(tel, id_vars=['benchmark', 'config'], value_vars='hw,kern,decache,decode,bind,emul,gc,fcall'.split(','))
+
+print(t)
+plt.figure(figsize=(12, 8))  # Width of 12 and height of 8
+ax = sns.histplot(t, x='benchmark', hue='variable', weights='value', multiple='stack')
+plt.savefig('sweep-results/telem.pdf', format='pdf')
