f565c16999058437ba726bd9dbd1d2f5d32d5ea3
HareshWed <hareshgw@gmail.com>
2025-08-13 14:59:47 -0500

    Fixed comparison decoding issues

--- arch/riscv64/decoder.c
+++ arch/riscv64/decoder.c
@@ -65,10 +65,10 @@ fpvm_inst_common_t capstone_to_common[RISCV_INS_ENDING] = {
     [RISCV_INS_FMIN_D] = {FPVM_OP_MIN, 0, 0, 8, 0},
     [RISCV_INS_FMAX_D] = {FPVM_OP_MAX, 0, 0, 8, 0},
 
-    // Compare Instructions
-    [RISCV_INS_FEQ_D] = {FPVM_OP_ADD, 0, 0, 8, 0},
-    [RISCV_INS_FLE_D] = {FPVM_OP_SUB, 0, 0, 8, 0},
-    [RISCV_INS_FLT_D] = {FPVM_OP_ADD, 0, 0, 8, 0},
+    //Compare instructions
+    [RISCV_INS_FEQ_D] = {FPVM_OP_CMPXX, 0, 0, 8, 0},
+    [RISCV_INS_FLE_D] = {FPVM_OP_CMPXX, 0, 0, 8, 0},
+    [RISCV_INS_FLT_D] = {FPVM_OP_CMPXX, 0, 0, 8, 0},
 
     // Fused Instructions 
     // (Matching x86 implementation of fused, so they might map to a different fused op)
@@ -236,7 +236,7 @@ static int decode_comparison(fpvm_inst_t *fi)
       ERROR("cmpxx operation but has no valid comparison type\n");
       return -1;
   }
-  return -1;
+  return 0;
 }
 
 int fpvm_decoder_init(void)
